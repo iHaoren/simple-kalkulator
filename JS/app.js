364420a90c33d1c08396610aeb40ddee9400db51
Ibnu Harun <ibnuharunalmudzakir@gmail.com>
2025-07-20 20:15:28 +0700

    initial commit

--- JS/app.js
+++ JS/app.js
@@ -3,23 +3,26 @@ let button = document.querySelectorAll("button");
 
 let string = "";
 
-button.forEach(element => {
-    element.addEventListener("click", (b) => {
-        if (b.target.innerText == "=") {
-            string = String(eval(string));
-            inputBox.value = string;
-        }else if (b.target.innerText == "AC") {
-            string = "";
-            inputBox.value = string;
-        }else if (b.target.innerText == "DEL") {
-            string = String.substring(0,string.length - 1);
-            inputBox.value =string;
-        }else if (b.target.innerText == "plusMin") {
-            string = String(-eval(string));
-            inputBox.value = string;
-        }else {
-            string += b.target.innerText;
-            inputBox.value = string;
-        }
-    })
-})
+button.forEach((element) => {
+  element.addEventListener("click", (b) => {
+    if (b.target.innerText == "=") {
+      string = String(eval(string));
+      inputBox.value = string;
+    } else if (b.target.innerText == "AC") {
+      string = "";
+      inputBox.value = string;
+    } else if (b.target.innerText == "DEL") {
+      string = string.substring(0, string.length - 1);
+      inputBox.value = string;
+    } else if (b.target.innerText == "plusMin") {
+      string = String(-eval(string));
+      inputBox.value = string;
+    } else if (b.target.innerText == "%") {
+        string = String(eval(string) /100);
+        inputBox.value = string;
+    } else {
+      string += b.target.innerText;
+      inputBox.value = string;
+    }
+  });
+});
